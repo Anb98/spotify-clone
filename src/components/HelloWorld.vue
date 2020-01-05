b95b354af035e72147523869749ce3dd4785bdf3
Abdiel Martinez <abdielantonio13@gmail.com>
2020-01-05 15:04:09 -0600

    tailwind added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+	<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" >
+		algo
+	</button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
