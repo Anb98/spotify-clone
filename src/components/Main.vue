8b31574b9f724862b12dd9e533ee5e3de34e0a27
Abdiel Martinez <abdielantonio13@gmail.com>
2020-01-16 23:36:22 -0600

    main section ok

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,54 @@
 <template>
 	<main class="bg-gray-800-spotify flex-1 overflow-y-auto" >
-		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium tempore autem in ratione nemo illo dolor tempora. Odio, veritatis quaerat cumque voluptatum numquam corporis optio perspiciatis officia repudiandae quod aperiam soluta natus sapiente itaque magnam nemo facilis provident distinctio iusto quibusdam enim excepturi exercitationem explicabo? Labore inventore earum recusandae tempora aliquid rem odit illum odio optio, debitis ducimus quisquam voluptatem illo aspernatur nostrum alias fugiat voluptate architecto reiciendis doloribus delectus possimus! Facilis vitae quaerat maiores magnam, excepturi consectetur ipsum cupiditate incidunt, et quasi amet tempore placeat, id eos deserunt similique. Iure harum optio beatae reiciendis saepe, numquam voluptatibus blanditiis, non doloribus vel soluta voluptatum eum quisquam eaque sequi voluptate nihil quia debitis? Sunt doloremque fugiat quibusdam quod asperiores aliquam quae itaque soluta debitis perferendis voluptate labore harum delectus incidunt tempora modi, similique minus, earum magnam quia laudantium! Itaque magni atque eveniet consectetur ducimus nam fugit enim veritatis accusantium dolorem deleniti, vitae, ullam labore. Est ut inventore similique eius minima laborum distinctio ullam culpa nam maxime illo impedit dolorem at molestias, natus recusandae quia reprehenderit dolores! Nostrum accusamus labore, facilis fugit aliquam harum consectetur temporibus. Ex impedit laboriosam eius, placeat deleniti maiores, nemo dolorum, praesentium voluptates quaerat ipsa vitae! Quos, iusto.
+		<div class="px-8 pb-4">
+			<h2 class="mt-20 text-4xl font-semibold ">Home</h2>
+			<Section title='Recently Played' :albums='albums' />
+			<Section title='Recently Played' :albums='albums' />
+		</div>
 	</main>
 </template>
 
 <script>
-export default {
+import Section from './Section';
 
+export default {
+	components:{
+		Section,
+	},
+	data:()=>({
+		albums:[
+			{
+				title:'Nada es imposible',
+				src:'http://1.bp.blogspot.com/-Bpcb6Yf5axw/Uuvsp4f-v6I/AAAAAAAAWMA/X51UVkhp7_E/s1600/1511285_633509676711465_1822919331_n.jpg',
+				description:'Nunca nada es imposible',
+				followers:9876,
+			},
+			{
+				title:'Nada es imposible',
+				src:'http://1.bp.blogspot.com/-Bpcb6Yf5axw/Uuvsp4f-v6I/AAAAAAAAWMA/X51UVkhp7_E/s1600/1511285_633509676711465_1822919331_n.jpg',
+				description:'Nunca nada es imposible',
+				followers:9876,
+			},
+			{
+				title:'Nada es imposible',
+				src:'http://1.bp.blogspot.com/-Bpcb6Yf5axw/Uuvsp4f-v6I/AAAAAAAAWMA/X51UVkhp7_E/s1600/1511285_633509676711465_1822919331_n.jpg',
+				description:'Nunca nada es imposible',
+				followers:9876,
+			},
+			{
+				title:'Nada es imposible',
+				src:'http://1.bp.blogspot.com/-Bpcb6Yf5axw/Uuvsp4f-v6I/AAAAAAAAWMA/X51UVkhp7_E/s1600/1511285_633509676711465_1822919331_n.jpg',
+				description:'Nunca nada es imposible',
+				followers:9876,
+			},
+			{
+				title:'Nada es imposible',
+				src:'http://1.bp.blogspot.com/-Bpcb6Yf5axw/Uuvsp4f-v6I/AAAAAAAAWMA/X51UVkhp7_E/s1600/1511285_633509676711465_1822919331_n.jpg',
+				description:'Nunca nada es imposible',
+				followers:9876,
+			},
+		]
+	})
 }
 </script>
 
---
+++ src/components/Section.vue
@@ -0,0 +1,28 @@
+<template>
+	<section class="mt-12">
+		<h3 class="font-semibold text-xl border-b border-gray-900 pb-2 ">Recently Played</h3>
+		<div class="flex items-center mt-4 -mx-4 flex-wrap ">
+			
+			<a v-for="(album, i) of albums" :key='i' href="#" class=" w-1/5 px-4">
+				<img :src="album.src" alt="cover album">
+				<div class="hover:underline">{{album.title}}</div>
+				<div class="text-gray-500 mt-1">{{album.description}}</div>
+				<div class="uppercase text-xs text-gray-500 mt-1">{{album.followers}} Followers</div>
+			</a>
+
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	props:{
+		title:{ type:String, required:true },
+		albums:{ type:Array, required:true }
+	}
+}
+</script>
+
+<style>
+
+</style>
